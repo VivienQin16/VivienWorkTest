efecec7f05eab696d2bede73afabd97e7cbcca1c
ting.qin <ting.qin@ecovacs.com>
2016-11-04 11:25:54 +0800

    Initial Commit

---
+++ VivienWorkTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VivienWorkTest
+//
+//  Created by Vivien on 16/11/4.
+//  Copyright © 2016年 Vivien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VivienWorkTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VivienWorkTest
+//
+//  Created by Vivien on 16/11/4.
+//  Copyright © 2016年 Vivien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
